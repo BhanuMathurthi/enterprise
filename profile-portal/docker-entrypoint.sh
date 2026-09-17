8ea798107073e9959e89506d656f24a0bd7aa0cb
Bhanu <saibhanu@orfus.in>
2026-09-17 21:19:52 +0530

    fix(deploy): bind Nginx to port 10000 on Render and support PORT env var

--- profile-portal/docker-entrypoint.sh
+++ profile-portal/docker-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -e
 
+LISTEN_PORT="${PORT:-80}"
 BACKEND_HOST="${BACKEND_URL:-http://user-management-service:8080}"
 
 # Strip trailing slash if present
@@ -12,7 +13,15 @@ case "$BACKEND_HOST" in
   *) BACKEND_HOST="http://${BACKEND_HOST}" ;;
 esac
 
+echo "Configuring Nginx to listen on port: ${LISTEN_PORT}"
+sed -i "s|listen 80;|listen ${LISTEN_PORT};|g" /etc/nginx/conf.d/default.conf
+sed -i "s|server_name localhost;|server_name _;|g" /etc/nginx/conf.d/default.conf
+
 echo "Configuring Nginx backend proxy target: ${BACKEND_HOST}/api/"
 sed -i "s|proxy_pass http://user-management-service:8080/api/;|proxy_pass ${BACKEND_HOST}/api/;|g" /etc/nginx/conf.d/default.conf
 
-exec "$@"
+if [ $# -eq 0 ]; then
+    exec nginx -g "daemon off;"
+else
+    exec "$@"
+fi
